2db670402f2acf15910442da63c118855c70e70f
Per Andersson <avtobiff@gmail.com>
2012-01-04 23:06:38 +0100

    Add copyright notice and GPLv3 license
    Blamed files to figure out copyright years, might be missing some (even
    contributors and copyright holders).
    
    Expanded contributors section in file headers where appropriate.
    
    Add GNU General Public License (version 3) in root directory.

--- compiler/erlang_check.erl
+++ compiler/erlang_check.erl
@@ -1,5 +1,27 @@
 #!/usr/bin/env escript
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2010 Pawel 'kTT' Salata
+% Copyright 2011 Ricardo Catalinas Jiménez
+%
+% This file is part of Vimerl.
+%
+% Vimerl is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Vimerl is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the-
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Vimerl.  If not, see <http://www.gnu.org/licenses/>.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 main([Filename]) ->
     Dir  = filename:dirname(Filename),
     Opts = [strong_validation,
